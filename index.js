0c8429217d7ea383f7ff0c0b8ea20c604e0a6a3c
shubmohan11 <30762257+shubmohan11@users.noreply.github.com>
2021-12-11 18:06:47 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,189 @@
+<html>
+    <head>
+        <title>Resume</title>
+<style>
+    body{
+        margin: 0px;
+    }
+   
+   #braand{
+    width: 40%;
+    height: 70px;
+    float: left;
+    text-align: center;
+    background-color: black;
+    color: white;
+   }
+  #menu{
+      width: 60%;
+      text-align: center;
+      float: right;
+      height: 70px;
+      background-color: black;
+      color: white;
+  }
+ 
+  h1{
+    line-height: 35px;
+  }
+  span{
+      color: white;
+      margin-right: 50px;
+      line-height: 70px;
+  }
+  
+  #image1{
+
+    border-radius: 50%;
+       margin-top: 150px;
+     }
+  #info{
+      float: right;
+      margin-top: 150px;
+      width:60% ;
+
+
+  }
+   #imgand{
+       width: 100%;
+   }
+   #imagdiv{
+            width: 40%;
+            text-align: center;
+            height: 400px;
+            float: left;
+   }
+   #table{
+       width: 100;
+       text-align: center;
+       
+   }
+   img{
+       border-radius: 50%;
+       margin-top: 150px;
+    margin-right: 100px;
+    margin-left: 50px;
+text-align: center;
+   }
+   h3{
+    margin-right: 100px;
+    margin-left: 150px;   
+   }
+   .fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+ 
+
+}
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+ 
+}
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+.fa-linkedin {
+  background: #55ACEE;
+  color: white;
+}
+#footers{
+   background-color: gray;
+   margin-top: 100px;
+}
+#last{
+    text-align: center;
+}
+#socials{
+    text-align: center;
+    margin-top: 50px;
+}
+a{
+    margin-top: 30px;
+}
+ 
+</style>
+    </head>
+    <body>
+       <div id="barsh">
+           <div id="braand">
+               <h1>My Portfolio</h1>
+           </div>
+           <div id="menu">
+               <span> <b> Home</b></span>
+               <span> <b> Profile</b></span>
+               <span><b> Resume</b></span>
+               <span><b> Contact</b></span>
+           </div>
+       </div> 
+
+<div id="imgand">
+    <div id="contents">
+     <div id="imagdiv">
+<img src="https://media-exp1.licdn.com/dms/image/C5603AQECTxENNRm46g/profile-displayphoto-shrink_800_800/0/1624362371703?e=1644451200&v=beta&t=sVc_Ss270N-gkyDmhJR01tO_Xzq7bOHZP3aGEpWqUsU" alt="Shubham" width="300px" height="300"
+ id="image1">
+     </div>
+     <div id="info">
+<h2>Hi There!!</h2>
+<h2>I am Shubham</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem debitis quam, tenetur nihil consectetur quas tempore quibusdam explicabo fuga consequuntur!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eum hic, incidunt expedita sequi eaque vitae, animi doloribus sunt quam, officiis nesciunt sapiente necessitatibus perferendis laborum fuga consequuntur accusantium. Ab nostrum architecto temporibus eos facilis sequi odit illo ipsam quibusdam.</p>
+<p><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eos, minima vel ex at sit quis tempora. Labore, magni asperiores!</b></p>
+     </div>
+    </div>
+    <div>
+<table>
+   <tbody>
+       <td>
+           <div><img src="https://images.pexels.com/photos/4065864/pexels-photo-4065864.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" width="300px" height="300">
+        <h3>Profile</h3></div>
+       </td>
+       <td>
+           <div><img src="https://images.pexels.com/photos/590016/pexels-photo-590016.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" width="300px" height="300">
+        <h3>Resume</h3></div>
+       </td>
+       <td>
+           <div>
+               <img src="https://images.pexels.com/photos/669615/pexels-photo-669615.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" width="300px" height="300">
+               <h3>Project</h3>
+           </div>
+       </td>
+       <td>
+           <div>
+               <img src="https://images.pexels.com/photos/33999/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" width="300px" height="300">
+               <h3>Contact</h3>
+           </div>
+       </td>
+   </tbody>
+ </table>
+    </div>
+
+</div>
+<div id="footers">
+
+<div id="socials">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-linkedin"></a>
+</div>
+<br>
+<div id="last">
+<p><b>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, esse mollitia distinctio praesentium consectetur ducimus debitis aperiam, suscipit incidunt laboriosam molestias nostrum dolorem et perferendis repellendus dignissimos deleniti sit odit. Aliquid, animi eligendi quasi, fugit cupiditate quia nam praesentium repellat omnis dolore qui culpa consectetur sint voluptatum itaque accusamus fugiat!</b></p>
+
+
+<h3>Copyright&copy; Reserved All Rights</h3>
+<h1>Shubham Mohan's Web Services Pvt.Ltd</h1>
+</div>
+
+</div>
+
+
+
+      
+    </body>
+</html>
